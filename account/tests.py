d27a5d6ff34fc4b7e9dde6358b60cbdffe7e17bc
nnagwek <43063766+nnagwek@users.noreply.github.com>
2020-02-14 15:00:08 +0530

    Adding test cases for Account
    Adding test cases for Account

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, Client, RequestFactory
+
+from django.contrib.auth.models import User
+
+class AccountTestCase(TestCase):
+
+    def setUp(self):
+        self.request_invalid_user_mock = {'username': 'abc', 'password': 'xyz'}
+        self.request_valid_user_mock = {'username': 'abcd', 'password': '1234'}
+        self.login_uri = '/accounts/login'
+
+    def test_loginInValid(self):
+        client = Client()
+        response = client.post(self.login_uri, self.request_invalid_user_mock)
+        self.assertEqual(response.status_code, 200)
+
+    def test_loginValid(self):
+        client = Client()
+        response = client.post(self.login_uri, self.request_valid_user_mock)
+        self.assertEqual(response.status_code, 200)
